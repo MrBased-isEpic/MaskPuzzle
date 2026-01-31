2db83bec52a6c37e52b58b14bc80dcdf55cbf3d4
Sri Kumaran <skisepic@gmail.com>
2026-01-31 11:09:55 +0530

    Pieces are draggable

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -1,8 +1,9 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class Piece : MonoBehaviour
+public class Piece : MonoBehaviour, IBeginDragHandler, IDragHandler, IDropHandler
 {
     public int id;
     
@@ -25,6 +26,7 @@ public class Piece : MonoBehaviour
 
     [SerializeField] public RectTransform rTransform;
     [SerializeField] private Image image;
+    [SerializeField] private Image shadow;
 
     // public bool isComplete
     // {
@@ -33,41 +35,41 @@ public class Piece : MonoBehaviour
     //         
     //     }
     // }
-    
 
+
+    private Vector2 dragOffset;
     public void SetIds(List<PieceData> pieces, int index)
     {
         id = index;
-        image.sprite = pieces[index].sprite;
-        rTransform.sizeDelta = new Vector2(image.sprite.rect.width, image.sprite.rect.height);
+        SetSprite(pieces[index].sprite);
         
         upId = -1;
         downId = -1;
         leftId = -1;
         rightId = -1;
         
-        Debug.Log($"Searching ids for {index}");
-        Debug.Log($"Position is {pieces[index].position}");
+        //Debug.Log($"Searching ids for {index}");
+        //Debug.Log($"Position is {pieces[index].position}");
         
         Vector2 upPos = new Vector2(pieces[index].position.x, 
             pieces[index].position.y + image.sprite.rect.height);
         
-        Debug.Log($"upPos is {upPos}");
+        //Debug.Log($"upPos is {upPos}");
         
         Vector2 leftPos = new Vector2(pieces[index].position.x - image.sprite.rect.width, 
             pieces[index].position.y);
         
-        Debug.Log($"leftPos is {leftPos}");
+        //Debug.Log($"leftPos is {leftPos}");
         
         Vector2 downPos = new Vector2(pieces[index].position.x, 
             pieces[index].position.y - image.sprite.rect.height);
         
-        Debug.Log($"downPos is {downPos}");
+        //Debug.Log($"downPos is {downPos}");
         
         Vector2 rightPos = new Vector2(pieces[index].position.x + image.sprite.rect.width, 
             pieces[index].position.y);
         
-        Debug.Log($"rightPos is {rightPos}");
+        //Debug.Log($"rightPos is {rightPos}");
         
         
         for (int i = index; i > 0; i--)
@@ -91,5 +93,30 @@ public class Piece : MonoBehaviour
             
             if (rightId != -1 && downId != -1) break;
         }
+    }
+
+    public void SetSprite(Sprite sprite)
+    {
+        shadow.sprite = sprite;
+        image.sprite = sprite;
+        
+        rTransform.sizeDelta = new Vector2(image.sprite.rect.width, image.sprite.rect.height);
+        shadow.GetComponent<RectTransform>().sizeDelta = rTransform.sizeDelta;
+        image.GetComponent<RectTransform>().sizeDelta = rTransform.sizeDelta;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rTransform.anchoredPosition = eventData.position - dragOffset;
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        dragOffset = eventData.position - rTransform.anchoredPosition;
     }
 }
--- Assets/Scripts/PieceManager.cs
+++ Assets/Scripts/PieceManager.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnPieces : MonoBehaviour
+public class PieceManager : MonoBehaviour
 {
     [SerializeField] private PuzzleSO puzzle;
     [SerializeField] private GameObject piecePrefab;
@@ -14,14 +14,14 @@ public class SpawnPieces : MonoBehaviour
     private void SpawnAllPieces()
     {
         List<PieceData> pieceData = puzzle.GetPieceData();
-        
-        Vector2 offset = new Vector2(puzzle.fullSprite.rect.width / 2, 
-            (puzzle.fullSprite.rect.height / 2) - pieceData[0].sprite.rect.height);
+
+        Vector2 offset = new Vector2((UnityEngine.Screen.width / 2) - (puzzle.fullSprite.rect.width / 2),
+            (UnityEngine.Screen.height / 2) - (puzzle.fullSprite.rect.height / 2));
 
         for (int i = 0; i < pieceData.Count; i++)
         {
             Piece piece = Instantiate(piecePrefab, this.transform).GetComponent<Piece>();
-            piece.rTransform.anchoredPosition = pieceData[i].position - offset;
+            piece.rTransform.anchoredPosition = pieceData[i].position + offset;
             piece.SetIds(pieceData, i);
         }
     }
